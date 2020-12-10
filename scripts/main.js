a4cf05f50a437923905112b297ab11282aa980c2
Abdu Umarov <abduumarov@MacBook-Pro-Abdu.local>
2020-12-10 12:25:12 +0500

    a

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+var openButton = document.querySelector ('#burger');
+var burgermenu = document.querySelector('#burger-menu')
+openButton. addEventListener ("click" , function (){
+    burgermenu.classList.toggle('opened');
+}) ;
